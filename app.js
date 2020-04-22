4c78dd3b4315691db152c68482abc5677b22a820
Sebastian Alarcon <salarcon@sistecredito.com>
2020-04-22 09:53:28 -0500

    eight commit

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 
 app.listen(3000, () => {
     console.log('server listening on 3000')
 })
-=======
-console.log('hello')
-console.log('aalarcon')
-console.log('sebas')
-console.log('ochoa')
-console.log('ochoa')
-console.log('ochoa')
-console.log('caro')
-console.log('hola')
-console.log('hola')
->>>>>>> 19c9f543515b1d802b2b15e4e81eb1a157e2d77c
